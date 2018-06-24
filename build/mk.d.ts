11a8ed7178be20e8e76e2ec8d295d8aaad675446
jolle <jolle@users.noreply.github.com>
2018-06-24 16:34:05 +0300

    Use a string instead of an array as the stack

--- build/mk.d.ts
+++ build/mk.d.ts
@@ -1,5 +1,3 @@
-declare module 'tini';
-
 export type MkArg = string | Object | HTMLElement;
 
 export function mk(query: string, ...MkArg): HTMLElement;
--- src/mk.js
+++ src/mk.js
@@ -8,7 +8,7 @@ module['exports']['mk'] = function() {
 
     var element;
 
-    let stack = [];
+    let stack = '';
     for (let i = 0; i <= selector.length; i++) {
         var current = selector[i];
         var bb = specialStarts.indexOf(current);
@@ -22,27 +22,26 @@ module['exports']['mk'] = function() {
                     ? stack[0] == specialStarts[bb - 1] // check that if the char is the ending char
                     : bb > -1)) // otherwise just check that the current char is a special char
         ) {
-            var n = stack.join(''),
-                b = n.substr(1);
+            var n = stack.substr(1);
             if (!element) {
                 // if there is no element already, we must be reading for the element type/name
-                element = e(n); // create new element with given name
-                stack = [current]; // start the new stack with current char as we were stopped by a special char
+                element = e(stack); // create new element with given name
+                stack = current; // start the new stack with current char as we were stopped by a special char
             } else if (stack[0] == '{' || stack[0] == '[') {
                 // check if the first char is an endable block
-                var ll1 = b.split('='); // split the current stack with =
+                var ll1 = n.split('='); // split the current stack with =
                 var ll = [ll1[0], ll1.slice(1).join('=')]; // make an array with the left side and right side of = (in case right side has more than one =)
                 if (stack[0] == '[') element.setAttribute(ll[0], ll[1]);
                 // if block is an attribute block, set the attribute
                 else element.style[ll[0]] = ll[1]; // otherwise presume that it's a style block; apply style
-                stack = []; // clear the stack
+                stack = ''; // clear the stack
             } else {
-                if (stack[0] == '.') element.classList.add(b);
-                else if (stack[0] == '#') element.id = b;
-                stack = [current];
+                if (stack[0] == '.') element.classList.add(n);
+                else if (stack[0] == '#') element.id = n;
+                stack = current;
             }
         } else {
-            stack.push(current);
+            stack += current;
         }
     }
 
