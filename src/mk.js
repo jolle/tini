16b94e3a2f767d2bdd4d14dd9ef341d96b4b7ff9
jolle <jolle@users.noreply.github.com>
2018-06-26 22:05:45 +0300

    Heavy optimization (503kb) and use better variable names

--- src/mk.js
+++ src/mk.js
@@ -1,65 +1,62 @@
-module['exports']['mk'] = function() {
+module['exports']['mk'] = function(selector) {
     var d = document;
     var e = d.createElement.bind(d);
-    var specialStarts = ['#', '.', '{', '}', '[', ']'];
-
-    var children = arguments;
-    var selector = children[0];
+    var specialStarts = '.{}[]~#'.split(''); // ~ is padding; when checking for block starters, indexes divisible by 2 are block starters
 
     var element;
 
     var stack = '';
-    var currentFirst = '';
+    var currentFirst;
     var isBlock; // falsish
     for (var i = 0; i <= selector.length; i++) {
         var current = selector[i];
-        var bb = specialStarts.indexOf(current);
-        var nc = bb && !(bb % 2);
-        var l = bb > -1;
+        var currentIndex = specialStarts.indexOf(current);
+        var currentIsStartingBlock = currentIndex % 2; // returns an integer but output can be 1 or 0; 1 is trueish and 0 is falseish
+        var isSpecialStart = ~currentIndex; // if not found, output is 0 (=falseish), otherwise -1 (=trueish)
 
-        if (!i && l) element = e('div');
+        if (!i && isSpecialStart) element = e('div'); // if it's the first iteration (i is falseish) and the first char is special, fall back to default element (div)
 
         if (
-            !current || // end
+            !current || // we use <= in the for cond so we can detect when the current doesn't exist -> we are at the end
             (isBlock // if the first char is an endable block...
-                ? currentFirst == bb - 1 // check that if the char is the ending char
-                : l) //) // otherwise just check that the current char is a special char
+                ? currentFirst == currentIndex - 1 // check that if the char is the ending char
+                : isSpecialStart) //) // otherwise just check that the current char is a special char
         ) {
             var n = stack.slice(1);
             if (!element) {
                 // if there is no element already, we must be reading for the element type/name
                 element = e(stack); // create new element with given name
             } else if (isBlock) {
-                // check if the first char is an endable block
+                // split by first =
                 var ll = n.split(/=(.*)/);
-                if (currentFirst == 4) element.setAttribute(ll[0], ll[1]);
-                // if block is an attribute block, set the attribute
-                else element.style[ll[0]] = ll[1]; // otherwise presume that it's a style block; apply style
-                current = '';
+                if (currentFirst % 3) element.style[ll[0]] = ll[1];
+                // the {'s index is not divisible by 3 (=trueish value)
+                else element.setAttribute(ll[0], ll[1]); // we'll presume that if it's not {, it's [
+                currentIsStartingBlock = currentIndex = current = ''; // resets everything and treats next round like a first round
             } else if (stack) {
-                if (currentFirst == 1) element.classList.add(n);
-                else element.id = n;
+                if (currentFirst) element.id = n;
+                // the class selector has index of 0 which is falseish and thus # has a trueish value
+                else element.classList.add(n); // we'll presuma that if it's not #, it's .
             }
-            isBlock = nc;
-            currentFirst = bb;
+            isBlock = currentIsStartingBlock;
+            currentFirst = currentIndex;
             stack = current;
         } else {
-            !isBlock && (isBlock = nc);
-            !currentFirst && (currentFirst = bb);
             stack += current;
         }
     }
 
-    for (var i = 1; i < children.length; i++) {
-        var o = children[i],
-            v = o.tagName;
-        if (o.call) {
-            element.addEventListener(o.name.slice(2), o);
+    for (var i = 1, arg = arguments; i < arg.length; i++) {
+        // starts at 1 as the first argument is the selector
+        var currentArg = arg[i];
+        if (currentArg.call) {
+            // o.call exists only on functions; not on HTMLElements nor strings/integers
+            element.addEventListener(currentArg.name.slice(2), currentArg);
         } else
             element.appendChild(
-                v // if a HTMLElement, will have a tagName
-                    ? o
-                    : d.createTextNode(o)
+                currentArg.tagName // if a HTMLElement, will have a tagName
+                    ? currentArg
+                    : d.createTextNode(currentArg)
             );
     }
 
